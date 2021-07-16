e91573cd297b4bd04d022c0bc37830f853b9c391
KKashpovski <kudimovaks@yandex.ru>
2021-07-17 00:53:32 +0300

    проходит tox приусловии, что комментирую в файте tox автотесты

---
+++ main.py
@@ -0,0 +1,33 @@
+"""Moon craters."""
+
+
+def calculate(matrix: list) -> int:
+    """Вычисление количества кратеров по исходным данным."""
+    counter = 0
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == 1:
+                if (i - 1 < 0 or matrix[i - 1][j] != 1) and (j - 1 < 0 or matrix[i][j - 1] != 1):
+                    counter = counter + 1
+            else:
+                counter = counter
+    print(counter)
+    return counter
+
+
+def main() -> None:
+    """Многомерный прямоугольный массив."""
+    with open("my_input.txt", "r") as fd:
+        moon_data = fd.read()
+        arr_a = []
+        for line in moon_data.split("\n"):
+            arr_tmp = []
+            for e in line.split(" "):
+                arr_tmp.append(int(e))
+            arr_a.append(arr_tmp)
+        print("Исходные данные:", arr_a)
+    calculate(arr_a)
+
+
+if __name__ == "__main__":
+    main()
